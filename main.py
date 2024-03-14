fc006707a6459b4dc2db65e6ee8efd4a2127618f
PYGOODS00 <163380128+PYGOODS00@users.noreply.github.com>
2024-03-14 19:33:38 +0600

    main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import streamlit
 import pyttsx3
 text = streamlit.text_input("EYT:")
-bot = pyttsx3.init()
-bot.say(text)
-bot.runAndWait()
+if streamlit.button("SUBMIT>"):
+  bot = pyttsx3.init()
+  bot.say(text)
+  bot.runAndWait()
+else:
+  streamlit.write("HI")
